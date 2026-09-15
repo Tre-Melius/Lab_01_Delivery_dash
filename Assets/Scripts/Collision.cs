8667c66eab698bf951804b34936dc1d49d83271b
Tre-Melius <melius569@morrisville.edu>
2026-09-14 20:48:08 -0400

    Moving our car/ removing controller support

---
+++ Assets/Scripts/Collision.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+ void OnCollisionEnter2d(Collision2D collision)
+ {
+    Debug.Log("You have entered the Realm of doom, death, and destruction");
+ 
+ }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,41 +3,39 @@ using UnityEngine.InputSystem;
 
 public class movement : MonoBehaviour
 {
-    [SerializeField] float movespeed = 5f;
-    [SerializeField] float rotationSpeed = 100f;
+    [SerializeField]float steerMove = 0.5f;
+    [SerializeField] float steerSpeed = 0.05f;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {    
-    }
-    //Rotation of the player object based on input
 
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal") * Time.deltaTime * movespeed; // This gives a value between -1 and +1 for the x-axis
-        float y = Input.GetAxis("Vertical") * Time.deltaTime * movespeed; // This gives a value between -1 and +1 for the y-axis
-        Vector3 Move = new Vector3(x, y, 0f); // Create a new vector for movement
         //transform.Rotate(0f, 0f, 0.4f); 
-        if (Input.GetKey(KeyCode.Q))
+        
+
+        float steer = 0f;
+        float move = 0f;
+
+        if (Keyboard.current.wKey.isPressed)
         {
-            transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+            move = 1f;
         }
-          if (Input.GetKey(KeyCode.E))
+        else if (Keyboard.current.sKey.isPressed)
         {
-            transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime);
+            move = -1f;
         }
-        transform.Translate(Move * movespeed*Time.deltaTime); // Move the player by adding the movement vector to the current position
-    }
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-            Debug.Log("Player collided with an obstacle!" + collision.gameObject.name);
-            // Handle collision with obstacle here
-            if (collision.gameObject.CompareTag("Respawn"))
-            {
-                // Handle collision with respawn object here
-                Debug.Log("Player collided with a respawn object!");
-            }
+        if (Keyboard.current.aKey.isPressed)
+        {
+            steer = 1f;
+        }
+        else if (Keyboard.current.dKey.isPressed)
+        {
+            steer = -1f;
+        }
+            
+        transform.Rotate(0, 0, steerSpeed);
+        transform.Translate(0, steerMove, 0);
         
     }
-   
 }
