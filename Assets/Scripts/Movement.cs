586100a89429ccce94ac3441172dfa719f38ff1a
Tre-Melius <melius569@morrisville.edu>
2026-09-22 16:21:10 -0400

    Finally got controller support to work

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,56 +3,27 @@ using UnityEngine.InputSystem;
 
 public class movement : MonoBehaviour
 {
-    [SerializeField]float steerMove = 0.5f;
-    [SerializeField] float steerSpeed = 0.05f;
+    [SerializeField] public float steerSpeed = 300f;
+    [SerializeField] public float moveSpeed = 5f;
 
-    private InputActionMap m_Map;
-    private InputAction m_Move;
-    private InputAction m_Turn;
+    public Vector2 moveInput;
+    public Rigidbody2D _rb;
 
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-
-    // Attempt to add controller support
-    void Awake()
+    void Start()
     {
-         m_Map = new InputActionMap("Player");
-
-
-        m_Map = m_Map.AddAction("Move", InputActionType.Value);
-         
     }
-    // Update is called once per frame
+
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
     void Update()
     {
-        //transform.Rotate(0f, 0f, 0.4f); 
-        
+        // moveInput.x = steering (left/right stick or A/D)
+        // moveInput.y = forward (up/down stick or W/S)
 
-        float steer = 0f;
-        float move = 0f;
-
-        if (Keyboard.current.wKey.isPressed)
-        {
-            move = 1f;
-        }
-        else if (Keyboard.current.sKey.isPressed)
-        {
-            move = -1f;
-        }
-        if (Keyboard.current.aKey.isPressed)
-        {
-            steer = 1f;
-        }
-        else if (Keyboard.current.dKey.isPressed)
-        {
-            steer = -1f;
-        }
-
-        float moveAmount = steer * steerSpeed * Time.deltaTime;
-        float steerAmount = move * steerMove * Time.deltaTime;
-            
-        transform.Rotate(0, 0, moveAmount);
-        transform.Translate(0, steerAmount, 0);
-        
+        transform.Rotate(0, 0, moveInput.x * steerSpeed * Time.deltaTime);
+        transform.Translate(transform.up * (moveInput.y * moveSpeed * Time.deltaTime));
     }
-}
+}   
