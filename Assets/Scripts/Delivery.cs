88723341e9764624165d3a303cc3317dec50fbd3
Tre-Melius <melius569@morrisville.edu>
2026-09-22 22:16:49 -0400

    Introducing Bools
    Fixed the Tags they now work, and bools are made

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
 
-public class Collision : MonoBehaviour
+public class Delivery : MonoBehaviour
 {
- void OnCollisionEnter2d(Collision2D collision)
+   bool hasPackage;
+ void OnTriggerEnter2D(Collider2D collision)
  {
-    Debug.Log("You have entered the Realm of doom, death, and destruction");
- 
- }
- void OnTrifferEnter2d(Collider2D collision)
- {
-    Debug.Log("You have made it through the realm");
+      if(collision.CompareTag("Package"))
+      {
+         Debug.Log("Picked up package");
+         hasPackage = true;
+      }
+
+      if(collision.CompareTag("Customer") && hasPackage)
+      {
+         Debug.Log("Delivered the package");
+         hasPackage = false;
+      }
  }
 }
