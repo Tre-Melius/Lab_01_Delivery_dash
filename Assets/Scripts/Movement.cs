02403a712dc65484f1a418b14ee15968126ffa17
Tre-Melius <melius569@morrisville.edu>
2026-09-22 21:18:35 -0400

    Add Collision blocks
    Removed  controller compatibility as it messed with controls

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,24 +6,41 @@ public class movement : MonoBehaviour
     [SerializeField] public float steerSpeed = 300f;
     [SerializeField] public float moveSpeed = 5f;
 
-    public Vector2 moveInput;
     public Rigidbody2D _rb;
 
     void Start()
     {
     }
 
-    public void OnMove(InputValue value)
-    {
-        moveInput = value.Get<Vector2>();
-    }
-
     void Update()
     {
+        float steer= 0;
+        float move= 0;
+        if (Keyboard.current.wKey.isPressed)
+        {
+            move = 1f;
+        }
+        else if(Keyboard.current.sKey.isPressed)
+        {
+            move = -1f;
+        }
+
+        if (Keyboard.current.aKey.isPressed)
+        {
+            steer = 1f;
+        }
+
+         else if(Keyboard.current.dKey.isPressed)
+        {
+            steer = -1f;
+        }
+
+        float moveAmount = move * moveSpeed * Time.deltaTime;
+        float steerAmount = steer * steerSpeed * Time.deltaTime;
         // moveInput.x = steering (left/right stick or A/D)
         // moveInput.y = forward (up/down stick or W/S)
 
-        transform.Rotate(0, 0, moveInput.x * steerSpeed * Time.deltaTime);
-        transform.Translate(transform.up * (moveInput.y * moveSpeed * Time.deltaTime));
+        transform.Rotate(0, 0, steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
 }   
